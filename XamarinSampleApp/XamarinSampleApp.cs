b4366c69f751ad6202984f6e9a7a5898468f8228
birmacher <birmacher@gmail.com>
2016-04-07 17:52:32 +0200

    iOS part wip

--- XamarinSampleApp/XamarinSampleApp.cs
+++ XamarinSampleApp/XamarinSampleApp.cs
@@ -9,17 +9,7 @@ namespace XamarinSampleApp
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			MainPage = new IntroCarousel();
 		}
 
 		protected override void OnStart ()
